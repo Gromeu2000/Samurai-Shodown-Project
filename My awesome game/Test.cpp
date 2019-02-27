9a63ef9234023a21945be914370ace2660a3be79
albertec1 <45201952+albertec1@users.noreply.github.com>
2019-02-27 12:51:50 +0100

    Rectengle movement test

---
+++ My awesome game/Test.cpp
@@ -0,0 +1,13 @@
+#include "SDL/include/SDL.h"
+#include <iostream>
+#pragma comment(lib, "SDL/SDL2main.lib")
+#pragma comment(lib, "SDL/SDL2.lib")
+
+int main(int argc, char*argv[])
+{
+
+	//screen dimension constants
+	const int SCREEN_WIDTH = 640;
+	const int SCREEN_HEIGTH = 480;
+
+}
